77e6e9b805e8af1d5bd9b9bcc24fc73f36e90adb
loren950603 <loren840603@gmail.com>
2022-08-24 19:12:57 -0400

    set up initial files

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+var router = require('express').Router();
+const passport = require('passport');
+
+// The root route renders our only view
+router.get('/', function(req, res) {
+  res.redirect('/students');
+});
+
+// Google OAuth login route
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope: ['profile', 'email'] }
+));
+
+// Google OAuth callback route
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect : '/students',
+    failureRedirect : '/students'
+  }
+));
+
+// OAuth logout route
+router.get('/logout', function(req, res){
+  req.logout(function(err) {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+module.exports = router;
